7b9ebc9cd135557d1e62f4f25c943d7f4feb8bf1
rubinaji <82864728+rubinaji@users.noreply.github.com>
2026-07-11 18:00:53 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+/*==================================================
+  RT DIGITAL - STARTER
+==================================================*/
+
+// Tunggu sampai seluruh dokumen HTML selesai dimuat di browser
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("RT Digital V1 Framework Ready!");
+  
+  // Jalankan rute pertama kali ke halaman dashboard
+  navigate("dashboard");
+});
